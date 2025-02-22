46ff8df5bf8718a65b66811601bb5504d6922c89
emre-aksu <emreaksu92@outlook.com>
2025-02-22 22:06:12 +0300

    Add age calculation from given birth year.

--- GitDemoProject/GithubConsoleApp/Program.cs
+++ GitDemoProject/GithubConsoleApp/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("doğum yılınız");
+int yil = Convert.ToInt32(Console.ReadLine());
+int age = DateTime.Now.Year - yil;
+Console.WriteLine("yaşınız: " + age);
+Console.ReadLine();
